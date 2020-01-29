bf1f80f72f06cc40bf27c09473579ecf1192dc74
glu000 <gl@gl.co.at>
2020-01-29 23:59:30 +0100

    Update

--- ics.php
+++ ics.php
@@ -10,11 +10,11 @@
  *
  * cdav uses Sabre/dav library http://sabre.io/dav/
  * Sabre/dav is distributed under use the three-clause BSD-license
- * 
+ *
  * Author : Befox SARL http://www.befox.fr/
  *
  ******************************************************************/
- 
+
 define('NOTOKENRENEWAL',1); 								// Disables token renewal
 if (! defined('NOLOGIN')) define('NOLOGIN','1');
 if (! defined('NOCSRFCHECK')) define('NOCSRFCHECK','1');	// We accept to go on this page from external web site.
@@ -25,16 +25,16 @@ function llxHeader() { }
 function llxFooter() { }
 
 function base64url_decode($data) {
-  return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
-} 
+	return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+}
 
 if(is_file('../main.inc.php'))
 	$dir = '../';
 elseif(is_file('../../../main.inc.php'))
 	$dir = '../../../';
-else 
+else
 	$dir = '../../';
-	
+
 require $dir.'main.inc.php';	// Load $user and permissions
 
 require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
@@ -79,6 +79,52 @@ $cdavLib = new CdavLib($user, $db, $langs);
 //Format them
 $arrEvents = $cdavLib->getFullCalendarObjects($id, true);
 
+echo "BEGIN:VCALENDAR\r\n";
+echo "VERSION:2.0\r\n";
+echo "METHOD:PUBLISH\r\n";
+echo "PRODID:-//Dolibarr CDav//FR\r\n";
+
+echo "X-PUBLISHED-TTL:PT5M\r\n";
+
+echo "BEGIN:VTIMEZONE\r\n";
+echo "TZID:Europe/Berlin\r\n";
+echo "BEGIN:DAYLIGHT\r\n";
+echo "TZOFFSETFROM:+0100\r\n";
+echo "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n";
+echo "DTSTART:19810329T020000\r\n";
+echo "TZNAME:GMT+2\r\n";
+echo "TZOFFSETTO:+0200\r\n";
+echo "END:DAYLIGHT\r\n";
+echo "BEGIN:STANDARD\r\n";
+echo "TZOFFSETFROM:+0200\r\n";
+echo "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n";
+echo "DTSTART:19961027T030000\r\n";
+echo "TZNAME:GMT+1\r\n";
+echo "TZOFFSETTO:+0100\r\n";
+echo "END:STANDARD\r\n";
+echo "END:VTIMEZONE\r\n";
+
+echo "BEGIN:VTIMEZONE\r\n";
+echo "TZID:Europe/Vienna\r\n";
+echo "BEGIN:DAYLIGHT\r\n";
+echo "TZOFFSETFROM:+0100\r\n";
+echo "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n";
+echo "DTSTART:19810329T020000\r\n";
+echo "TZNAME:GMT+2\r\n";
+echo "TZOFFSETTO:+0200\r\n";
+echo "END:DAYLIGHT\r\n";
+echo "BEGIN:STANDARD\r\n";
+echo "TZOFFSETFROM:+0200\r\n";
+echo "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n";
+echo "DTSTART:19961027T030000\r\n";
+echo "TZNAME:GMT+1\r\n";
+echo "TZOFFSETTO:+0100\r\n";
+echo "END:STANDARD\r\n";
+echo "END:VTIMEZONE\r\n";
+
+$anz = 0;
+
+
 foreach($arrEvents as $event)
 {
 	if ($type == 'nolabel')
@@ -87,8 +133,28 @@ foreach($arrEvents as $event)
 		$event['calendardata'] = preg_replace('#SUMMARY:.*[^\n]#', 'SUMMARY:'.$langs->trans('Busy'), $event['calendardata']);//FIXME translate busy !!
 		$event['calendardata'] = preg_replace('#DESCRIPTION:.*[^\n]#', 'DESCRIPTION:.', $event['calendardata']);
 		$event['calendardata'] = preg_replace('#LOCATION:.*[^\n]#', 'LOCATION:', $event['calendardata']);
+
+		$event ['calendardata'] = preg_replace('~\R~u', "\r\n", $event ['calendardata']);
+
 		echo $event['calendardata'];
+
 	}
 	else
+	{
+		$event ['calendardata'] = preg_replace('~\R~u', "\r\n", $event ['calendardata']);
 		echo $event['calendardata'];
+	}
+	$anz++;
+}
+
+echo "END:VCALENDAR\r\n";
+
+loginfo ("User: " . $id . ", Anz.Events: " . $anz);
+
+function loginfo ($text)
+{
+	$myfile = fopen("/var/lib/dolibarr/documents/cdav.log", "a+") or die("Unable to open file!");
+
+	fwrite($myfile,date("Y-m-d H:i:s",time()).": " . $text . "\n");
+	fclose($myfile);
 }
