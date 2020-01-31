5df1b27e5ddb493911f8f802473888630e14e4a3
glu000 <gl@gl.co.at>
2020-01-31 02:02:10 +0100

    Update CalDAVDolibarr.php

--- class/CalDAVDolibarr.php
+++ class/CalDAVDolibarr.php
@@ -200,7 +200,7 @@ class Dolibarr extends AbstractBackend implements SyncSupport, SubscriptionSuppo
 				'{urn:ietf:params:xml:ns:caldav}calendar-description'                => trim($row['firstname'].' '.$row['lastname']),
 				'{urn:ietf:params:xml:ns:caldav}calendar-timezone'                   => date_default_timezone_get(),
 				'{http://apple.com/ns/ical/}calendar-order'                          => $row['rowid']==$this->user->id?0:$row['rowid'],
-				'{http://apple.com/ns/ical/}calendar-color'                          => ($row['color']=='#2952A3')?'':('#'.$row['color']),
+				'{http://apple.com/ns/ical/}calendar-color'                          => ($row['color']=='2952A3')?'':('#'.$row['color']),
 
 				// try unorthodox method :
 				'{http://calendarserver.org/ns/}subscribed-strip-todos'              => false,
